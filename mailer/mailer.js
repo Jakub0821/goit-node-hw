6898e80781c7477ffe01e1bdd149fda49551722a
Jakub0821 <jakubmigda120@gmail.com>
2024-05-06 21:06:57 +0200

    change app.js, package-lock.json, package.json and  mailer.js

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,23 +1,14 @@
-const sendgridMail = require("@sendgrid/mail");
-sendgridMail.setApiKey(process.env.SENGRID_API_KEY);
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere'});
 
-const sendVerificationEmail = async (userEmail, next, verificationToken) => {
-    const verificationMassage = {
-        to: `${userEmail}`,
-        // to: `jakubmigda120@gmail.com`, // NOTE: uncontect is for test
-        from: `jakubmigda120@gmail.com`,
-        subject: `Confirm your email address`,
-    text: `Click on the link to confirm your account: http://localhost:3000/api/users/verify/${verificationToken}`,
-    html: `<strong>Click on the link</strong> to confirm your account: http://localhost:3000/api/users/verify/${verificationToken}`,
-    };
-    
-    try {
-      const email = await sendgridMail.send(verificationMessage);
-      return email;
-    } catch (error) {
-      console.error("Something went wrong: ", error);
-      next(error);
-    }
-  };
-  
-  module.exports = { sendVerificationEmail };
+mg.messages.create('sandbox824fad29a4c64387beb828bc7d6631ea.mailgun.org', {
+	from: "Mailgun Sandbox <postmaster@sandbox824fad29a4c64387beb828bc7d6631ea.mailgun.org>",
+	to: ["jakubmigda120@gmail.com"],
+	subject: "Hello",
+	text: "Testing some Mailgun awesomeness!",
+	html: "<h1>Testing some Mailgun awesomeness!</h1>"
+})
+.then(msg => console.log(msg)) // logs response data
+.catch(err => console.log(err)); // logs any error
